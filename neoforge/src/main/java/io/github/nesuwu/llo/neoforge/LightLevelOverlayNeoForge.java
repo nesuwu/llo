7b8b4feff6a3b6270068bab95639bbe8858735e4
Ellen Marie Dash <me@duckie.co>
2026-05-06 21:47:37 -0400

    If llo is running on a server, bail immediately in the LightLevelOverlayNeoForge constructor.

--- neoforge/src/main/java/io/github/nesuwu/llo/neoforge/LightLevelOverlayNeoForge.java
+++ neoforge/src/main/java/io/github/nesuwu/llo/neoforge/LightLevelOverlayNeoForge.java
@@ -3,9 +3,11 @@ package io.github.nesuwu.llo.neoforge;
 import io.github.nesuwu.llo.ClientConfig;
 import io.github.nesuwu.llo.ClothConfigScreens;
 import io.github.nesuwu.llo.LightLevelOverlayClient;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
@@ -14,6 +16,11 @@ import net.neoforged.neoforge.common.NeoForge;
 public class LightLevelOverlayNeoForge {
 
     public LightLevelOverlayNeoForge() {
+        if (FMLEnvironment.dist == Dist.DEDICATED_SERVER) {
+            // Nothing to do on a server.
+            return;
+        }
+
         ClientConfig.ensureLoaded();
         LightLevelOverlayClient.init();
         NeoForge.EVENT_BUS.register(this);
